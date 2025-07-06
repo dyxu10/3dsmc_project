b3aa622d872b69f31fd94cb74b5f6f40de2aad42
root <453662843@qq.com>
2025-07-06 19:14:32 +0000

    Cleaned Penglin branch: removed all files

--- DPHM-main/DPHM-main/scripts/preprocess/dataset_creation/camera/util/typing.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-
-def to_base_tensor(tensor: torch.Tensor):
-    # TODO: Not sure if this always works
-    tensor.__class__ = torch.Tensor
-    return tensor
-    # cast = torch.DoubleTensor if tensor.dtype == torch.float64 else torch.FloatTensor
-    # return cast(tensor)
